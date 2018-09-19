c8aa5c462ad7c5fea8c570f0a6cd3dc7129eb0b8
Sabrina <sabrina.larouche1998@gmail.com>
2018-09-19 13:29:42 -0400

    Ex1: Added image under the mouse

--- excercises/exercise1/js/script.js
+++ excercises/exercise1/js/script.js
@@ -27,6 +27,15 @@ var rectY;
 var rectWidth;
 var rectHeight;
 
+//The image of the butterfly
+
+var buttImage;
+
+// Current position of the butterfly image
+
+var buttImageX;
+var buttImageY;
+
 // preload()
 //
 // Load the two images we're using before the program starts
@@ -34,6 +43,8 @@ var rectHeight;
 function preload() {
   clownImage = loadImage("assets/images/clown.png");
   feltTextureImage = loadImage("assets/images/black-felt-texture.png");
+// Added the butterfly image
+  buttImage = loadImage("assets/images/butterfly.png")
 }
 
 
@@ -46,8 +57,11 @@ function setup() {
   createCanvas(640,640);
 
   // Start the clown image at the centre of the canvas
-  clownImageX = width/2;
-  clownImageY = height/2;
+  //clownImageX = width/2;
+  //clownImageY = height/2;
+
+  buttImageX = width/2;
+  buttImageY = height/2;
 
   // Start the felt image perfectly off screen above the canvas
   //feltTextureImageX = width/2;
@@ -56,7 +70,7 @@ function setup() {
   rectY = 0;
 
   rectWidth = 200;
-  rectHeight = 200;
+  rectHeight = 640;
   // We'll use imageMode CENTER for this script
   imageMode(CENTER);
 }
@@ -81,12 +95,25 @@ function draw() {
   // Move the clown by moving it 1/10th of its current distance from the mouse
 
   // Calculate the distance in X and in Y
-  var xDistance = mouseX - clownImageX;
-  var yDistance = mouseY - clownImageY;
+  //var xDistance = mouseX - clownImageX;
+  //var yDistance = mouseY - clownImageY;
+
+  //Calculate the distance in X and Y
+  var xDistance = mouseX - buttImageX;
+  var yDistance = mouseY - buttImageY;
+
   // Add 1/10th of the x and y distance to the clown's current (x,y) location
-  clownImageX = clownImageX + xDistance/10;
-  clownImageY = clownImageY + yDistance/10;
+  //clownImageX = clownImageX + xDistance/10;
+  //clownImageY = clownImageY + yDistance/10;
+
+// Add 1/10th of the x and y distance to the clown's current (x,y) location
+  buttImageX = buttImageX + xDistance/10;
+  buttImageY = buttImageY + yDistance/10;
 
   // Display the clown image
-  image(clownImage,clownImageX,clownImageY);
-}
+  //image(clownImage,clownImageX,clownImageY);
+  // }
+
+//Display butterfly image
+  image(buttImage,buttImageX,buttImageY);
+  }

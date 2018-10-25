5607a6906f2fa7b71c80b241a34293c12e0f570b
Sabrina <sabrina.larouche1998@gmail.com>
2018-10-25 18:42:49 -0400

    Ex6: Reset instead of rest

--- excercises/exercise6/js/Ball.js
+++ excercises/exercise6/js/Ball.js
@@ -78,7 +78,8 @@ Ball.prototyp.handleCollision = function((paddle) {
 // reset()
 //
 // Set position back to the middle of the screen
-Ball.prototype.rest = function () {
+//////////////// FIXED
+Ball.prototype.reset = function () {
   this.x = width/2;
   this.y = height/2;
 }

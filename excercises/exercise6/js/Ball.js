a4191a33ecaf77de184250bc313b238a39b48fe5
Sabrina <sabrina.larouche1998@gmail.com>
2018-10-31 14:26:43 -0400

    Ex6: Added width and height to rect

--- excercises/exercise6/js/Ball.js
+++ excercises/exercise6/js/Ball.js
@@ -58,7 +58,8 @@ Ball.prototype.isOffScreen = function () {
 // Draw the ball as a rectangle on the screen
 Ball.prototype.display = function () {
 //////////////// FIXED
-  rect(this.x,this.y);
+  //////////////// FIXED
+  rect(this.x,this.y,this.size,this.size);
 }
 
 // handleCollision(paddle)

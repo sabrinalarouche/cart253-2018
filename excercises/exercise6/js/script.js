01ed257cb50f6baae38e61c215fe964cdc8fd4ac
Sabrina <sabrina.larouche1998@gmail.com>
2018-10-31 14:39:56 -0400

    Ex6: Height is 60 not 600

--- excercises/exercise6/js/script.js
+++ excercises/exercise6/js/script.js
@@ -26,7 +26,8 @@ function setup() {
   //////////////// FIXED
   ball = new Ball(width/2,height/2,50,50,10,50,50);
   // Create the right paddle with UP and DOWN as controls
-  rightPaddle = new Paddle(width-10,height/2,10,600,10,UP_ARROW,DOWN_ARROW);
+  //////////////// FIXED
+  rightPaddle = new Paddle(width-10,height/2,10,60,10,UP_ARROW,DOWN_ARROW);
   // Create the left paddle with W and S as controls
   // Keycodes 83 and 87 are W and S respectively
   //////////////// FIXED

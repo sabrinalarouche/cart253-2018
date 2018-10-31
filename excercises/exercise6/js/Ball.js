42698782826a8c4a6b7ec320f4d821b0be606059
Sabrina <sabrina.larouche1998@gmail.com>
2018-10-31 13:34:16 -0400

    Ex6: missing comma

--- excercises/exercise6/js/Ball.js
+++ excercises/exercise6/js/Ball.js
@@ -55,7 +55,8 @@ Ball.prototype.isOffScreen = function () {
 //
 // Draw the ball as a rectangle on the screen
 Ball.prototype.display = function () {
-  rect(this.x this.y);
+//////////////// FIXED
+  rect(this.x,this.y);
 }
 
 // handleCollision(paddle)

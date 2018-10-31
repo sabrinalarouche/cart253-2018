ba7033dd183aaceab53c06a62a59704fcf7fb58e
Sabrina <sabrina.larouche1998@gmail.com>
2018-10-31 13:44:43 -0400

    Ex6: prototyp changed to prototype

--- excercises/exercise6/js/Ball.js
+++ excercises/exercise6/js/Ball.js
@@ -64,7 +64,8 @@ Ball.prototype.display = function () {
 //
 // Check if this ball overlaps the paddle passed as an argument
 // and if so reverse x velocity to bounce
-Ball.prototyp.handleCollision = function((paddle) {
+//////////////// FIXED
+Ball.prototype.handleCollision = function((paddle) {
   // Check if the ball overlaps the paddle on x axis
   if (this.x + this.size > paddle.x && this.x < paddle.x + paddle.w) {
     // Check if the ball overlaps the paddle on y axis
